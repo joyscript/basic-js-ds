653f5e03112bb1d91f30221898c8f7bfcb566245
joyscript <joyscript7@gmail.com>
2023-04-13 06:55:51 +0300

    feat: solve "binary-search-tree"

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,97 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+
+const getDir = (data, node) => (data < node.data ? 'left' : 'right');
+
 class BinarySearchTree {
+  constructor() {
+    this.base = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.base;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!this.base) {
+      this.base = new Node(data);
+      return;
+    }
+
+    let cur = this.base;
+
+    while (true) {
+      if (cur.data === data) return;
+      let dir = getDir(data, cur);
+      if (!cur[dir]) {
+        cur[dir] = new Node(data);
+        return;
+      }
+      cur = cur[dir];
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let cur = this.base;
+    while (cur) {
+      if (cur.data === data) return cur;
+      let dir = getDir(data, cur);
+      cur = cur[dir];
+    }
+    return null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return !!this.find(data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.base = replaceNode(this.base, data);
+
+    function replaceNode(node, data) {
+      if (!node) return null;
+
+      if (data !== node.data) {
+        let dir = getDir(data, node);
+        node[dir] = replaceNode(node[dir], data);
+        return node;
+      }
+
+      if (!node.left && !node.right) return null;
+      if (!node.left) return node.right;
+      if (!node.right) return node.left;
+
+      let maxLeft = node.left;
+      while (maxLeft.right) maxLeft = maxLeft.right;
+      node.data = maxLeft.data;
+      node.left = replaceNode(node.left, maxLeft.data);
+
+      return node;
+    }
+  }
+
+  findExtremum(dir) {
+    if (!this.base) return null;
+    let cur = this.base;
+    while (cur[dir]) cur = cur[dir];
+    return cur.data;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.findExtremum('left');
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.findExtremum('right');
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
